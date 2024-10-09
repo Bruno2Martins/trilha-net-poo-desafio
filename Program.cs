d9f20fbde8b75db7fd6ab86e3252cbdf4124ee64
BrunoMartins <mart02br@gmail.com>
2024-10-09 10:03:40 -0300

    commit implementando todos os TODOs requisitados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n = new Nokia(numero: "123321",modelo: "NokiaTijolo",imei: "00012",memoria: 128);
+Console.WriteLine("Seu número: "+n.Numero);
+n.Numero = "332211";
+Console.WriteLine("Novo número: "+n.Numero);
+//n.Modelo = "nokia 1"; - inacessivel
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("Instagram");
+
+Console.WriteLine("__________");
+
+Iphone ip = new Iphone(numero: "098797",modelo: "IPhone 8",imei: "02018",memoria: 256);
+Console.WriteLine("Seu número: "+ip.Numero);
+ip.Numero = "990191";
+Console.WriteLine("Novo número: "+ip.Numero);
+ip.Ligar();
+ip.ReceberLigacao();
+ip.InstalarAplicativo("Spotify");
